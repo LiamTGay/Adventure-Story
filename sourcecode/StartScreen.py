b4627fb79c0c19ffa9781e20cb577e6a8973ebae
goober42 <142818683+goober42@users.noreply.github.com>
2024-03-08 11:36:39 -0700

    Add files via upload

---
+++ sourcecode/StartScreen.py
@@ -0,0 +1,22 @@
+# Import module  
+from tkinter import *
+
+# Create object  
+root = Tk() 
+
+# Adjust size  
+root.geometry("400x400") 
+
+class  Backgrounds:
+  def __init__(self):
+    pass
+
+  def Start(self):
+
+    bg = PhotoImage(file = "startscreen.png") 
+
+    root.mainloop()
+
+  def Part1(self):
+    pass
+  
